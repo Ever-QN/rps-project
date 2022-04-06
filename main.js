bed72337037f99e526d4bc0e8fb1ce6430838285
Ever-QN <QuynhTNgo@live.com>
2022-04-05 18:13:47 -0600

    Finish playRound() function

--- main.js
+++ main.js
@@ -1,7 +1,54 @@
 function computerPlay() {
-    const gameChoices = ["Rock", "Paper", "Scissors"];
-    let randomResult = gameChoices[Math.floor(Math.random() * gameChoices.length)];
-    return randomResult;
+    const gameChoices = ["rock", "paper", "scissors"];
+    let computerResult = gameChoices[Math.floor(Math.random() * gameChoices.length)];
+    return computerResult;
 }
 
-computerPlay();
+function playRound (playerSelection, computerSelection) {
+
+    let roundResult;
+
+    if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper") {
+
+        roundResult = "You lose. Paper beats rock.";
+        return roundResult;
+
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
+        
+        roundResult = "You win! Rock beats scissors";
+        return roundResult;
+
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
+
+        roundResult = "You win! Paper beats rock."
+        return roundResult;
+
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") {
+
+        roundResult = "You lose. Scissors beats paper.";
+        return roundResult;
+
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection == "rock") {
+
+        roundResult = "You lose. Rock beats scissors.";
+        return roundResult;
+
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection == "paper") {
+
+        roundResult = "You win! Scissors beats paper.";
+        return roundResult;
+
+    } else if (playerSelection.toLowerCase() === computerSelection) {
+
+        roundResult = "Tied! Both of you chose " + playerSelection + ".";
+        return roundResult;
+
+    } else {
+
+        roundResult = "Error! Something went wrong. Perhaps an invalid input?";
+        return roundResult;
+
+    }
+
+} 
+
